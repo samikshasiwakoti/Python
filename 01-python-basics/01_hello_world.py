7e076df41df406ea33ec12c6a5f97c4a27dbc174
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-08-24 17:34:34 +0530

    Organize Python basics

--- 01-python-basics/01_hello_world.py
+++ 01-python-basics/01_hello_world.py
@@ -1,1 +1,3 @@
-
+print("hello,Python!")
+print("I am learning Python.")
+print("I want to become a python backend developer.")
--- 01_basic/01_hellowrold.py
+++
@@ -1,3 +0,0 @@
-print("hello,Python!")
-print("I am learning Python.")
-print("I want to become a python backend developer.")
