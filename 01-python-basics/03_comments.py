4e7f7009a85aa74d4ca5af05d105bc9fe2e9e4b6
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-08-24 18:33:33 +0530

    Learning python comments

--- 01-python-basics/03_comments.py
+++ 01-python-basics/03_comments.py
@@ -1,1 +1,11 @@
+# hi i am learning comment
+name = "bhawana"
+
+""" this is the age of bhawana
+ i think she will gonna be pro in python"""
+
+age =18
+# this is adress
+address = "jhapa"
+print(f"Hi i am {name}  and my age is {age}. I am from{address}")
 
