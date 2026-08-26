f817931d124907cf4501dd224dbf81776239d077
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-08-26 22:13:54 +0530

    Learn about input function in Python

---
+++ 01-python-basics/userinput.py
@@ -0,0 +1,11 @@
+# input() is built in function, it takes data from user
+# num1 = input("Enter first number:")
+# num2 = input("Enter second number:")
+# print(num1+num2) it gives string cause input() always give string
+a = int(input('Enter your first number'))
+b = int(input('Enter your second number'))
+print( a + b)
+
+c= float(input('Enter your third number'))
+d = int(input('Enter your fouth number'))
+print( c + d)
