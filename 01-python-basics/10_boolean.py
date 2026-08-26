72af1eb40ec729d7bc5ff8c8aa1442655457fe66
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-08-26 21:11:14 +0530

    Learn python boolean datatype

---
+++ 01-python-basics/10_boolean.py
@@ -0,0 +1,13 @@
+age = 20
+print(age>18)
+print(age<18)
+print(age==20)
+
+print(bool(2))
+print(bool("hi"))
+
+# 0 gives false and all non zero values give 
+# empty string - false
+print(bool(''))
+print(bool([1,2,3]))
+print(bool([]))
