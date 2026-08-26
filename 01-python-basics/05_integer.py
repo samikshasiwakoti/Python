369c94b19be1bdf521d10a67d81fc79043dca52c
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-08-26 13:38:18 +0530

    Learn integer data types

--- 01-python-basics/05_integer.py
+++ 01-python-basics/05_integer.py
@@ -1,1 +1,65 @@
+<<<<<<< HEAD
+=======
+age4 = 23
+marks1 = 85
+temperature = -5
+year = 2026 # basically this all is datatypes
+
+print(type(age4))
+
+# the process of converting one datatypes to another is called type conversion
+# string to int 
+age1= " 20"
+age2= int(age1)
+print(age2)
+print(type(age2))
+
+age3 = int("25")
+print(age3)
+print(type(age3))
+
+# float to int
+price = 1.1
+number = int(price)
+print(number)
+
+# price(int(-5.9)) only psoctive float is calculated
+# basically string can convert only string with valid whole number into int
+
+age = 20
+marks = 80
+number_of_siblings = 2
+
+print(f"My age is {age}. My marks is {marks} and i have {2} siblings")
+
+a = 25
+b = 10
+print(f"the value of a and b are {a} and {b}")
+print(type(a))
+print(type(b))
+
+student_name = "Bhawana"
+student_age = "18"
+student_roll = "25"
+print(int(student_age))
+print(int(student_roll))
+
+number = int("50")
+
+print(number)
+print(type(number)) # here number will be 50 and type is int
+
+# number = int("Python")  it gives error
+# it is possible to convert int to float,float to int, int to str,str to int and flort to str, str to float
+
+number1 = 10.5
+result1 = str(number1)
+print(type(result1))
+
+number2 = 10
+result2 = float(number2)
+print(type(result2))
+print(result2)
+
+>>>>>>> 159c4a5 (Learn integer data types)
 
