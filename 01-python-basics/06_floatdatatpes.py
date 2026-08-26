d2a3fe5f9a5b294979b4136bf460b9cf49d94b69
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-08-26 14:14:49 +0530

    Learn python float datatypes

---
+++ 01-python-basics/06_floatdatatpes.py
@@ -0,0 +1,26 @@
+price = 99.99
+temperature = 25.5
+height = 5.6
+
+print(type(price))
+print(int(height))
+
+age = 20
+result = float(age)
+print(result)
+print(type(result))
+
+# number = "python"
+# result1 = float(number)
+# print(result1) it gives error cause it is not numeric value of string
+
+
+price = "99.50"
+result2 = float(price)
+print(result2)
+
+
+notebook_cost = 50.50
+quantity_brought = 3
+total = notebook_cost * quantity_brought
+print(total)
